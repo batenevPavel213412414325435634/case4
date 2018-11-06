0ad6737a2c146d0535096e2808d75161858f5147
batenevPavel213412414325435634 <batenev.pasha@mail.ru>
2018-11-06 10:23:27 +0700

    sentens,words,syllables

---
+++ main.py
@@ -0,0 +1,23 @@
+"""Case-study #4 Анализ текста
+Разработчики:
+Батенев П.А., Григорьев А.Е., Долгих Н.А.
+"""
+
+text = input("Введите текст:")
+count_sentens = text.count(".") + text.count("?") + text.count("!")
+number = text.split()
+count_words = len(number)
+text_ = text.lower()
+count_syllables = text_.count("а") + text_.count("е") + text_.count("о") + text_.count("у") + text_.count("э") \
+                  + text_.count("ю") + text_.count("я") + text_.count("и") + text_.count('ы')
+
+ASL = None
+ASW = None
+FRE = None
+print('Предложений:', count_sentens)
+print('Слов:', count_words)
+print('Слогов:', count_syllables)
+print('Средняя длина предложения в словах:', ASL)
+print('Средняя длина слова в слогах:', ASW)
+print('Индекс удобочитаемости Флеша:', FRE)
+
